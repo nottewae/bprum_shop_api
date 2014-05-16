c763c7ac4a1979143b25bf4c0e8c293f4b9c6c16
nottewae <nottewae@gmail.com>
2014-05-16 14:07:21 +0400

    all

--- lib/bprum_shop_api.rb
+++ lib/bprum_shop_api.rb
@@ -1,35 +1,41 @@
-require "bprum_shop_api/version"
-
+#require "bprum_shop_api/version"
+require 'bcrypt'
+require 'json'
 module BprumShopApi
   class Api
 
-    def initialize(my_key,remote_key,remote_path,remote_host)
-      @my_key=my_key
-      @remote_key=remote_key
-      @remote_path=remote_path
-      @remote_host=remote_host
+    def initialize(my_key,remote_key,remote_path,remote_host,charset='utf-8')
+      @my_key = my_key
+      @remote_key = remote_key
+      @remote_path = remote_path
+      @remote_host = remote_host
+      @charset=charset
     end
 
-    def checkRequest(params)
-      params["sign"]=nil
-      mysign=Digest::SHA2.hexdigest(params.to_json+@my_key)
-      if mysign==params["sign"]
+    def checkRequest(respond)
+      parsed=JSON.parse(respond)
+      arr=parsed["request_body"].sort
+      order_hash=arr.to_h
+      mysign=Digest::SHA2.hexdigest(order_hash.to_json+@my_key)
+      if mysign==respond["sign"]
         true
       else
         false
       end
     end
     def signRequest(params)
-      Digest::SHA2.hexdigest(params.to_json+@remote_key)
+      arr=params[:request_body].sort
+      params[:request_body]=arr.to_h
+      Digest::SHA2.hexdigest(params[:request_body].to_json+@remote_key)
     end
     def reguestProcessor(params)
       hash=params
-      hash["sign"]=signRequest(params)
+      hash[:sign]=signRequest(params)
       hash=hash.to_json
       result={}
       Net::HTTP.start(@remote_host) do |http|
         req = Net::HTTP::Post.new(@remote_path)
-        req.set_content_type('text/json', { 'charset' => 'utf-8' })
+        req.set_content_type('text/json', { 'charset' => @charset })
         req.body = hash
         response = http.request(req)
         if response.class.to_s == 'Net::HTTPOK'
