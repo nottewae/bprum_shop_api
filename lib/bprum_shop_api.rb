944104199a73bc4cdada45d04653e5972010e3e1
nottewae <nottewae@gmail.com>
2014-05-16 13:07:02 +0400

    api func

--- lib/bprum_shop_api.rb
+++ lib/bprum_shop_api.rb
@@ -2,8 +2,49 @@ require "bprum_shop_api/version"
 
 module BprumShopApi
   class Api
-    def initialize
-      puts "Yahoo"
+
+    def initialize(my_key,remote_key,remote_path,remote_host)
+      @my_key=my_key
+      @remote_key=remote_key
+      @remote_path=remote_path
+      @remote_host=remote_host
+    end
+
+    def checkRequest(params)
+      params["sign"]=nil
+      mysign=Digest::SHA2.hexdigest(params.to_json+@my_key)
+      if mysign==params["sign"]
+        true
+      else
+        false
+      end
+    end
+    def signRequest(params)
+      Digest::SHA2.hexdigest(params.to_json+@remote_key)
+    end
+    def reguestProcessor(params)
+      hash=params
+      hash["sign"]=signRequest(params)
+      hash=hash.to_json
+      result={}
+      Net::HTTP.start(@remote_host) do |http|
+        req = Net::HTTP::Post.new(@remote_path)
+        req.set_content_type('text/json', { 'charset' => 'utf-8' })
+        req.body = hash
+        response = http.request(req)
+        if response.class.to_s == 'Net::HTTPOK'
+          puts response.inspect
+          puts response.value
+          result = JSON.parse(response.body)
+        else
+          puts response.inspect
+        end
+      end
+      if result["access"]=="granted"
+        #TODO Написать обработчик
+      else
+        return false
+      end
     end
   end
 end
